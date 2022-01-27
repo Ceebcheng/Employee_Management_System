180ed31d8980d58d5f71008a815daa9a80ce80b1
Ceebcheng <jonathan2075@gmail.com>
2022-01-27 15:55:45 -0600

    added qury, functions, and prompts

---
+++ index.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: process.env.user,
+      password: process.env.password,
+      database: process.env.database
+    },
+    console.log(`Connected to the company_db database.`)
+  );
+
+const listofOptions = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "Please select one.",
+            name: "listofOptions",
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee" , "update an employee role"]
+
+        }
+    ])
+
+    .then(userChoice => {
+        switch (userChoice.listofOptions) {
+            case "view all departments":
+                viewallDepartments();
+                break;
+            case "view all roles":
+                viewallRoles();
+                break;
+            case "view all employees":
+                viewallEmployees();
+                break;
+            case "add a department":
+                addDepartment();
+                break;
+            case "add a role":
+                addRole();
+                break;
+            case "add an employee":
+                addEmplyoee();
+                break;
+            case "update an employee role":
+                updateemloyeeRole();
+                break;
+            default: viewallDepartments();
+        }
+    });
+};
+
+function viewallDepartments() {
+    const sql = `SELECT * from department`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+        console.log(err);
+       return;
+    }
+    console.table(rows);
+  });
+}
+function viewallRoles() {
+    const sql = `SELECT department.name, role.id, role.title, role.salary FROM role JOIN department ON role.department_id = department.id`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+        console.log(err);
+       return;
+    }
+    console.table(rows);
+  });
+}
+function viewallEmployees() {
+    console.log('test3');
+}
+function addDepartment() {
+    console.log('viewallDepartments');
+}
+function addRole() {
+    console.log('viewallDepartments');
+}
+function addEmplyoee() {
+    console.log('viewallDepartments');
+}
+function updateemloyeeRole() {
+    console.log('viewallDepartments');
+}
+
+
+
+listofOptions();
